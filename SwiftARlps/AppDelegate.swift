798ad32e1339eb5294cbb52ac311ce5c645a718c
buddybuild <help@buddybuild.com>
2017-11-21 01:26:15 -0800

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- SwiftARlps/AppDelegate.swift
+++ SwiftARlps/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        BuddyBuildSDK.setup()
+        
         guard ARWorldTrackingConfiguration.isSupported else {
             fatalError("""
                 ARKit is not available on this device. For apps that require ARKit
