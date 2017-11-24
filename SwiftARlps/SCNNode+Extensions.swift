9db858b49891d06b42778eb5db21345fcdc92d79
Bojan Markovic <bolo992.apple@hotmail.com>
2017-11-24 15:19:17 +0100

    Implemented changing scale for some objects.

---
+++ SwiftARlps/SCNNode+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  SCNNode+Extensions.swift
+//  SwiftARlps
+//
+//  Created by Bojan Markovic  on 11/24/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+
+extension SCNNode {
+    func changeScaleIfPhysicsBodyIncluded(forSize size: CGFloat) {
+        self.physicsBody = nil
+        self.scale = SCNVector3(size, size, size)
+        self.physicsBody = .dynamic()
+    }
+}
