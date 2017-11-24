5cd51448720aca458ffb1b8a265b22dffe78d15b
Meghan Kane <kanemeghan@gmail.com>
2017-11-24 16:06:12 +0100

    Change color of selected object to red

---
+++ SwiftARlps/SCNNode+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  SCNNode+Extensions.swift
+//  SwiftARlps
+//
+//  Created by Bojan Markovic  on 11/24/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+
+extension SCNNode {
+    func changeScaleIfPhysicsBodyIncluded(forSize size: CGFloat) {
+        self.physicsBody = nil
+        self.scale = SCNVector3(size, size, size)
+        self.physicsBody = .dynamic()
+    }
+    
+    func changeColor(color: UIColor) {
+        let material = getMaterial(for: color)
+        self.geometry?.materials = [material]
+    }
+}
--- SwiftARlps/ViewController.swift
+++ SwiftARlps/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var addCubeButton: UIButton!
     @IBOutlet weak var addSphereButton: UIButton!
+    @IBOutlet weak var changeColorToRedButton: UIButton!
 
     @IBOutlet weak var blurView: UIVisualEffectView!
     
@@ -56,6 +57,12 @@ class ViewController: UIViewController {
     /// Convenience accessor for the session owned by ARSCNView.
     var session: ARSession {
         return sceneView.session
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func didTapRedButton() {
+        virtualObjectInteraction.selectedObject?.changeColor(color: UIColor.red)
     }
     
     // MARK: - View Controller Life Cycle
@@ -160,6 +167,7 @@ class ViewController: UIViewController {
             addObjectButton.isHidden = true
             addCubeButton.isHidden = true
             addSphereButton.isHidden = true
+            changeColorToRedButton.isHidden = true
             return
         }
         
@@ -176,6 +184,7 @@ class ViewController: UIViewController {
         addObjectButton.isHidden = false
         addCubeButton.isHidden = false
         addSphereButton.isHidden = false
+        changeColorToRedButton.isHidden = false
         statusViewController.cancelScheduledMessage(for: .focusSquare)
 	}
     
